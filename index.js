3d4a1da49adec8952ebbda0b71599d3808e92cb7
c5403 <james.p.mcshane@healthpartners.com>
2017-11-23 00:51:38 -0600

    Adding datastore

--- index.js
+++ index.js
@@ -1,28 +1,90 @@
 'use strict';
 
 const twilio = require('twilio');
+const uuidv4 = require('uuid/v4');
 const runtimeConfig = require('cloud-functions-runtime-config');
 const MessagingResponse = twilio.twiml.MessagingResponse;
 
 const projectId = process.env.GCLOUD_PROJECT;
 const region = 'us-central1';
 const authToken = runtimeConfig.getVariable('dev-config', 'TWILIO_AUTH_TOKEN');
+const isLive = runtimeConfig.getVariable('dev-config', 'IS_LIVE');
+const datastore = require('@google-cloud/datastore')();
 
 let tokenValue = "";
+let isAppRunning = "";
 authToken.then((val) => tokenValue = val);
+isLive.then((val) => isAppRunning = val);
 
-exports.reply = (req, res) => {
-  let isValid = true;
+function buildResultKey() {
+  return datastore.key(['PokerResult', uuidv4()]);
+}
+function buildPlayerKey(number) {
+  return datastore.key(['PokerPlayer', number]);
+}
 
-  // Only validate that requests came from Twilio when the function has been
-  // deployed to production.
-  /*if (process.env.NODE_ENV === 'production') {
-    isValid = twilio.validateExpressRequest(req, tokenValue, {
+function savePlayer(requestBody, res) {
+  const key = buildPlayerKey(requestBody.From);
+  const playerName = requestBody.Body.replace(/my name is\s*/i,'');
+  const entity = {
+    key: key,
+    data: {name : playerName}
+  }
+  datastore.save(entity)
+      .then(() => {
+        console.log("Saved complete");
+        endResponse(res, 200);
+      })
+      .catch((err)=> {
+        console.error(err);
+        console.log("Save failed");
+        endResponse(res, 500);
+      });
+}
+
+function savePokerResult(requestBody, res) {
+  const key = buildResultKey();
+  const playerKey = buildPlayerKey(requestBody.From);
+
+  datastore.get(playerKey)
+    .then(([entity]) => {
+      if (!entity) {
+        throw new Error(`No entity found for key ${requestBody.From}.`);
+      }
+      const resultEntity = {
+        key: key,
+        data: {message : requestBody.Body, time: new Date(), player: entity.name}
+      };
+      return datastore.save(resultEntity);
+    })
+    .then(() => {
+      console.log("Saved complete");
+      endResponse(res, 200);
+    })
+    .catch((err) => {
+      console.error(err);
+      endResponse(res, 500);
+    });
+
+}
+function validate() {
+  if (isAppRunning === 'Running') {
+    return twilio.validateExpressRequest(req, tokenValue, {
       url: `https://${region}-${projectId}.cloudfunctions.net/reply`
     });
-  }*/
-  // Halt early if the request was not sent from Twilio
-  if (!isValid) {
+  } else {
+    return true;
+  }
+}
+
+function endResponse(res, status) {
+  res
+    .status(status)
+    .end();
+}
+
+exports.reply = (req, res) => {
+  if (!validate()) {
     res
       .type('text/plain')
       .status(403)
@@ -30,16 +92,9 @@ exports.reply = (req, res) => {
       .end();
     return;
   }
-
-  // Prepare a response to the SMS message
-  const response = new MessagingResponse();
-
-  // Add text to the response
-  response.message('Hello from Google Cloud Functions!');
-
-  // Send the response
-  res
-    .status(200)
-    .type('text/xml')
-    .end(response.toString());
+  if (req.body.Body.toLowerCase().indexOf("my name") > -1) {
+    savePlayer(req.body, res);
+  } else {
+    savePokerResult(req.body, res);
+  }
 };
